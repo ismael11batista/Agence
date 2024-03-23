1be3efe2d0d49e8b9af61c95ebc46b41d326bc7f
ismael11batista <139612335+ismael11batista@users.noreply.github.com>
2024-03-22 23:34:14 -0300

    Add files via upload

--- outboundtext/script.js
+++ outboundtext/script.js
@@ -31,20 +31,116 @@ function copiarTextoParaClipboard() {
     });
 }
 
-// O restante do seu código permanece inalterado
+function removerLinhasPorInicio(texto, iniciosParaRemover) {
+    // Divide o texto em linhas para processamento
+    let linhas = texto.split('\n');
+    // Filtra as linhas, removendo aquelas que começam com algum dos inícios especificados
+    linhas = linhas.filter(linha => !iniciosParaRemover.some(inicio => linha.startsWith(inicio)));
+    // Reconstitui o texto com as linhas restantes
+    return linhas.join('\n');
+}
+
+function removerTermosEspecificos(texto, termosParaRemover) {
+    termosParaRemover.forEach(termo => {
+        // Usando expressão regular para substituir o termo por uma string vazia globalmente, ignorando maiúsculas e minúsculas
+        texto = texto.replace(new RegExp(termo, 'gi'), '');
+    });
+    return texto;
+}
+
 
 
 function ajustarTexto() {
     let texto = document.getElementById('inputText').value;
 
     // Remover dias da semana em CAIXA ALTA
-    const diasSemana = ['SEGUNDA-FEIRA', 'TERÇA-FEIRA', 'QUARTA-FEIRA', 'QUINTA-FEIRA', 'SEXTA-FEIRA', 'SÁBADO', 'DOMINGO'];
+    const diasSemana = ['SEGUNDA-FEIRA', 'TERÇA-FEIRA', 'QUARTA-FEIRA', 'QUINTA-FEIRA', 'SEXTA-FEIRA', 'SÁBADO', 'DOMINGO', 'HOJE'];
     diasSemana.forEach(dia => {
         texto = texto.replace(new RegExp(dia + '\\n', 'g'), '');
     });
 
     // Remover Emojis (uma vez é suficiente)
     texto = texto.replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{1FB00}-\u{1FBFF}\u{1FC00}-\u{1FCFF}\u{1FD00}-\u{1FDFF}\u{1FE00}-\u{1FEFF}\u{1FF00}-\u{1FFFF}]+/gu, '');
+
+    const iniciosParaRemover = [
+        "Nenhum selecionado",
+        "Como usar o E-mail de Agence.com.br ",
+        "Conversa aberta.",
+        "Pular para o conteúdo",
+        "De: Felipe Santos",
+        "Enviada em:",
+        "Para: ",
+        "Assunto:",
+        "ATENÇÃO:  Esta mensagem é de REMETENTE EXTERNO",
+        "*** NOVO *** NÃO digite sua SENHA WEG",
+        "Negócios Especiais  •",
+        "AGENCE CONSULTING – CORPORATE ",
+        "Brasil – São Paulo - SP",
+        "Brasil – Campo Grande",
+        "www.agence.com.br  |  www.agence.global",
+        "LEGAL NOTICE",
+        "Caixa de entrada",
+        "Externa",
+        "Importante",
+        " ",
+        "1 de",
+        "RES: ",
+        "HOJE",
+        "[Mensagem cortada]  Exibir toda a mensagem",
+        "_______________",
+        "Obter o Outlook para Android",
+        "Enviado: ",
+        "Sent: ",
+        "From: Felipe H. Santos",
+        "Email já foi Respondido",
+        "Resposta positiva",
+        "Re: ",
+        "To: ",
+        "Subject: ",
+        "De: Felipe H",
+        "...",
+        "  ",
+        "para mim",
+        "Área de anexos",
+        'De: "Felipe Santos"',
+        'Enviadas: ',
+        'Resposta negativa',
+        'This message',
+        // Adicione mais inícios conforme necessário
+        '"Esta mensagem',
+        '"This message',
+        'seg.,',
+        'ter.,',
+        'qua., ',
+        'qui., ',
+        'sex., ',
+
+    ];
+
+
+
+    // Termos específicos a serem removidos
+    const termosParaRemover = [
+        "Conexão de 1º grau",
+        "· 1º",
+        "felipe.santos@agence.com.br",
+        "Atenciosamente,",
+        "Felipe H. Santos",
+        'ZjQcmQRYFpfptBannerStart',
+        'ZjQcmQRYFpfptBannerEnd',
+        'Book time',
+        'From: Felipe Santos <>',
+
+
+        // Adicione mais termos conforme necessário
+    ];
+
+
+
+    texto = removerLinhasPorInicio(texto, iniciosParaRemover);
+
+    texto = removerTermosEspecificos(texto, termosParaRemover);
+
 
     // Remover linhas que começam com "Ver perfil de"
     texto = texto.split('\n').filter(linha => !linha.startsWith('Ver perfil de')).join('\n');
